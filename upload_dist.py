9f3e3ddb17864c1b4d4bb69e78e97d4310a6424f
KhacTu <juicycactuz@gmail.com>
2024-10-26 13:16:56 +0200

    Upload dist script

---
+++ upload_dist.py
@@ -0,0 +1,7 @@
+import os
+
+api_token = os.environ['pypi_api_token']
+print(api_token)
+
+os.system('py -m build')
+os.system(f'py -m twine upload --repository pypi dist/* --username token --password {api_token}')
